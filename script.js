060d55a41795fe2e762064066844b41f4dca9c26
muhammet ergun <ergu0005@algonquinlive.com>
2020-09-30 16:35:16 -0400

    done

---
+++ script.js
@@ -0,0 +1,25 @@
+//additional function
+function add(a, b){
+return a+ b
+}
+
+//subtraction function 
+function subtract(a, b){
+return a - b    
+}
+
+//multiplaction function
+function multiply(a, b){
+return a * b    
+}
+
+//devision function
+function devide(a, b){
+return a / b
+}
+
+//big long calculation
+let result = add(subtract(1, 2), multiply(3, 4))
+//    add( -1 ,   12  )
+
+console.log(result)
